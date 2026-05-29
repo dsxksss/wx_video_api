2d4506862ac048408c7df454371919c47c249a85
VentCat <2546650292@qq.com>
2026-05-29 15:31:17 +0800

    bump: 升级 SDK 版本号至 1.2.0

--- wx_video_sdk/__init__.py
+++ wx_video_sdk/__init__.py
@@ -2,8 +2,10 @@ from wx_video_sdk.client import WXVideoClient
 from wx_video_sdk.assistant import WXVideoAssistant
 from wx_video_sdk.models import AppConfig
 
+__version__ = "1.2.0"
+
 # Generic name for easier identification in main.py if needed, 
 # although using specific classes is better for developers.
 WXVideoSDK = WXVideoClient
 
-__all__ = ["WXVideoClient", "WXVideoAssistant", "AppConfig", "WXVideoSDK"]
+__all__ = ["WXVideoClient", "WXVideoAssistant", "AppConfig", "WXVideoSDK", "__version__"]
